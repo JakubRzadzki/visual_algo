69a17244bd0f7028e7b0c47d34239d76192c9c12
Jakub Rządzki <jakub.rz456@gmail.com>
2026-05-17 00:23:04 +0200

    chore: codebase cleanup and removal of deprecated files
    - Delete deprecated TypeScript BST implementation (src/algorithms/bst.ts)
    - Remove obsolete scratch/ directory containing layout and store test scripts

--- scratch/test_treeLayout.ts
+++
@@ -1,9 +0,0 @@
-import { computeLayoutD3 } from '../src/utils/treeLayout';
-const root = {
-  id: '1',
-  value: 10,
-  left: { id: '2', value: 5, left: null, right: null },
-  right: { id: '3', value: 15, left: null, right: null }
-};
-const positions = computeLayoutD3(root as any);
-console.log("Positions:", positions);
